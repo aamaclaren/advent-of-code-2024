b283beacb66001a200911d6fa56873ba2c14ad6b
Drew MacLaren <aamaclaren@gmail.com>
2024-12-13 10:29:43 -0500

    updated template

--- _template/day01.js
+++ _template/day01.js
@@ -4,9 +4,13 @@ const input = fs.readFileSync("./input.txt").toString();
 
 
 //part 1
+console.time('Part 1');
 
 console.log(`Part 1:`);
+console.timeEnd('Part 1');
 
 //part 2
-
+console.time('Part 2');
 console.log(`Part 2:`);
+
+console.timeEnd('Part 2');
